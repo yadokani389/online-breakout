965e649771e9f99a267e4661737ddf5f5aa4dcb2
yadokani389 <yadokani389@gmail.com>
2025-05-11 11:41:29 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,149 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      rust-overlay,
+      ...
+    }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      imports = with inputs; [
+        git-hooks.flakeModule
+        treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          toolchain = pkgs.rust-bin.stable.latest.default;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ inputs.rust-overlay.overlays.default ];
+          };
+
+          packages.default = rustPlatform.buildRustPackage {
+            pname = "online-breakout";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            nativeBuildInputs = with pkgs; [
+              makeWrapper
+              pkg-config
+            ];
+
+            buildInputs = with pkgs; [
+              zstd
+              alsa-lib
+              udev
+              vulkan-loader
+              wayland
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr
+            ];
+
+            postFixup =
+              with pkgs;
+              lib.optionalString stdenv.hostPlatform.isLinux ''
+                patchelf $out/bin/online-breakout \
+                  --add-rpath ${
+                    lib.makeLibraryPath [
+                      libxkbcommon
+                      vulkan-loader
+                    ]
+                  }
+              '';
+          };
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              config.pre-commit.devShell
+            ];
+            inherit (config.packages.default) nativeBuildInputs buildInputs;
+
+            packages = with pkgs; [
+              clang
+            ];
+
+            LD_LIBRARY_PATH =
+              with pkgs;
+              lib.makeLibraryPath [
+                libxkbcommon
+                vulkan-loader
+                udev
+                alsa-lib
+              ];
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+              taplo.enable = true;
+            };
+
+            settings.formatter = {
+              taplo.options = [
+                "fmt"
+                "-o"
+                "reorder_keys=true"
+              ];
+            };
+          };
+
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              hooks = {
+                ripsecrets.enable = true;
+                typos.enable = true;
+                treefmt.enable = true;
+                cargo-check.enable = true;
+                clippy = {
+                  enable = true;
+                  packageOverrides.cargo = toolchain;
+                  packageOverrides.clippy = toolchain;
+                };
+              };
+            };
+          };
+        };
+    };
+}
